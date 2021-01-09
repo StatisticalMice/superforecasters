8491032254ceb920b39b256ca5c17e7c525c0467
Ari <ari.huttunen@me.com>
2021-01-09 13:50:38 +0200

    download csv file from github

--- analyysi-pluto.jl
+++ analyysi-pluto.jl
@@ -7,24 +7,30 @@ using InteractiveUtils
 # ╔═╡ bd3a666c-51c4-11eb-0ea5-3beaf9b624c2
 begin
   import Pkg
+  # Adding packages is required for deployment on Binder
   Pkg.add("CSV")
   Pkg.add("DataFrames")
   Pkg.add("PlutoUI")
   Pkg.add("StatsPlots")
-  using PlutoUI, CSV, DataFrames, StatsPlots
+  Pkg.add("RemoteFiles")
+  using PlutoUI, CSV, DataFrames, StatsPlots, RemoteFiles
   plotly()
 end
-
-# ╔═╡ b9a5cb92-51d6-11eb-3f9c-7fc268d1cc5a
-@__DIR__
 
 # ╔═╡ 3a965f9c-510e-11eb-08dc-8d1cdc52e218
 with_terminal() do
 	Pkg.status()
 end
 
+# ╔═╡ 30e459ae-526c-11eb-19b3-8f67825515c2
+begin
+	@RemoteFile data_csv "https://raw.githubusercontent.com/StatisticalMice/superforecasters/main/data2020_b.csv"
+	download(data_csv, verbose=true)
+	csv_path = path(data_csv)
+end
+
 # ╔═╡ 0cdb58ae-510f-11eb-12d4-5393fb3dec0c
-df_raw = CSV.read("data2020_b.csv", DataFrame)[!,3:end];
+df_raw = CSV.read(csv_path, DataFrame)[!,3:end];
 
 # ╔═╡ 3493c430-51b9-11eb-2b59-6bd9bde30ed5
 
@@ -69,11 +75,13 @@ begin
 			column_names[n] = t
 		end
 	end	
-	select!(df, :nimi, :tyyppi, :Q1, :Q2, :Q3, :Q5, :)
 end;
 
 # ╔═╡ 93552228-5189-11eb-0a42-5776e305433f
 column_names
+
+# ╔═╡ 824e0940-526f-11eb-383a-915bba34cc29
+df
 
 # ╔═╡ 3006417c-5187-11eb-0d0c-1bcaaf659578
 clean_column_name("27.  ")
@@ -87,14 +95,12 @@ function string_with_na_to_float(s)
     parse(Float32, s)
 end
 
-# ╔═╡ 06e2bc36-5186-11eb-3a97-65b85b57388d
-df2 = select(df, 
-	1:6, 
-	names(df, 7:32) .=> ByRow(string_with_na_to_float),
-	renamecols=false)
-
-# ╔═╡ 9312c0c2-5189-11eb-2b25-57488418a6a2
-describe(df2, :first, :median, :mean, :eltype, :nmissing)
+# ╔═╡ cddd9434-526f-11eb-3510-bd2b0159f846
+df2 = select(df, :nimi, :tyyppi,
+	intersect(names(df, r"^Q"), names(df, Float64)),
+	intersect(names(df, r"^Q"), names(df, String)) .=> ByRow(string_with_na_to_float),
+	renamecols=false
+)
 
 # ╔═╡ 35013218-51b9-11eb-275e-d5a3685e2ea0
 begin
@@ -103,6 +109,9 @@ begin
 	xaxis!(column_names[:Q1])
 end
 
+# ╔═╡ e936638a-51dd-11eb-01b5-672579b31497
+describe(df2, :first, :median, :mean, :eltype, :nmissing)
+
 # ╔═╡ 8e371412-5194-11eb-221f-152be903cc6e
 string_with_na_to_float("20")
 
@@ -110,15 +119,16 @@ string_with_na_to_float("20")
 string_with_na_to_float("NA")
 
 # ╔═╡ Cell order:
-# ╠═b9a5cb92-51d6-11eb-3f9c-7fc268d1cc5a
+# ╠═35013218-51b9-11eb-275e-d5a3685e2ea0
 # ╠═bd3a666c-51c4-11eb-0ea5-3beaf9b624c2
 # ╠═3a965f9c-510e-11eb-08dc-8d1cdc52e218
+# ╠═30e459ae-526c-11eb-19b3-8f67825515c2
 # ╠═0cdb58ae-510f-11eb-12d4-5393fb3dec0c
 # ╠═93b434d4-5189-11eb-32be-e97d67e6f555
 # ╠═93552228-5189-11eb-0a42-5776e305433f
-# ╠═06e2bc36-5186-11eb-3a97-65b85b57388d
-# ╠═9312c0c2-5189-11eb-2b25-57488418a6a2
-# ╠═35013218-51b9-11eb-275e-d5a3685e2ea0
+# ╠═824e0940-526f-11eb-383a-915bba34cc29
+# ╠═cddd9434-526f-11eb-3510-bd2b0159f846
+# ╠═e936638a-51dd-11eb-01b5-672579b31497
 # ╠═3493c430-51b9-11eb-2b59-6bd9bde30ed5
 # ╠═9ebdd936-5187-11eb-2ed8-3fa4ec45d80c
 # ╠═c9f9c422-5185-11eb-3e8d-3f96592baac1
